152a0e01457b41696764f331e7fc06e9a2aa8204
alexandre <aobellianne@phoceis.asia>
2018-07-11 18:03:14 +0800

    Fix srcObject for Safari

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,50 @@
 <template>
   <div id="app">
-    <vc-cam></vc-cam>
+    <vc-cam ref="camera" :width="400" :height="400"></vc-cam>
+    <button @click="snapshot">Snap My Photo</button>
+    <img class="thumb" :src="photo"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      photo: null
+    }
+  },
+  methods: {
+    snapshot () {
+      this.photo = this.$refs.camera.snapshot()
+      /* eslint-disable */
+      console.log(this.photo)
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .thumb {
+    width: 200px;
+    height: 200px;
+    object-fit: contain;
+    border: solid 1px black;
+    background-color: gainsboro;
+    border-radius: 100px;
+  }
+
+  .vc-video {
+    border: solid 1px red;
+    border-radius: 200px;
+  }
 </style>
--- src/components/Cam.vue
+++ src/components/Cam.vue
@@ -1,17 +1,115 @@
 <template>
   <div>
-      <h1>This is cam</h1>
+      <video ref="video" class="_vc-video vc-video" :style="videoStyle" :width="width" :height="height" autoplay></video>
+      <canvas ref="canvas" :width="width" :height="height" v-show="false"></canvas>
   </div>
 </template>
 
 <script>
+/*eslint-disable*/
+
 export default {
   name: 'Cam',
   props: {
+    width: {
+      type: Number,
+      default: 640
+    },
+    height: {
+      type: Number,
+      default: 480
+    },
+    fit: {
+      type: String,
+      default: 'cover',
+      validator: function (value) {
+        return ['cover', 'contain'].indexOf(value) !== -1
+      }
+    }
+  },
+  computed: {
+    videoStyle () {
+      return {
+        'object-fit': this.fit
+      }
+    }
+  },
+  methods: {
+    calculateVideoPreviewSize () {
+      let video = this.$refs.video
+      var videoPreviewSize = {
+        width: video.videoWidth,
+        height: video.videoHeight,
+      }
+
+      let previewRatio = this.width / this.height
+      let videoRatio = video.videoWidth / video.videoHeight
+
+      // Set video preview size with width as a reference
+      let widthFirst = () => {
+        videoPreviewSize.width = this.width
+        videoPreviewSize.height = video.videoHeight * this.width / video.videoWidth
+      }
+
+      // Set video preview size with height as a reference
+      let heightFirst = () => {
+        videoPreviewSize.height = this.height
+        videoPreviewSize.width = video.videoWidth * this.height / video.videoHeight
+      }
+
+      if (this.fit == 'contain') {
+        if ((previewRatio < 1 && videoRatio < 1) || (previewRatio <= 1 && videoRatio >= 1)) {
+          widthFirst()
+        } else {
+          heightFirst()
+        }
+      } else if (this.fit == 'cover') {
+        if ((previewRatio < 1 && videoRatio < 1) || (previewRatio <= 1 && videoRatio >= 1)) {
+          heightFirst()
+        } else {
+          widthFirst()
+        }
+      }
+
+      return videoPreviewSize
+    },
+    snapshot () {
+      var video = this.$refs.video
+      var canvas = this.$refs.canvas
+      var context = canvas.getContext('2d')
+
+      var videoPreviewSize = this.calculateVideoPreviewSize()
+
+      // Draw the image
+      let topOffset = (this.height - videoPreviewSize.height) / 2
+      let leftOffset = (this.width - videoPreviewSize.width) / 2
+      context.drawImage(video, leftOffset, topOffset, this.width - leftOffset * 2, this.height - topOffset * 2)
+
+      // Return a base 64 snapshot
+      return canvas.toDataURL()
+    }
+  },
+  mounted () {
+    var video = this.$refs.video
+
+    // Get permission to use the camera
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia ({
+          video: true
+        }
+      )
+      .then (stream => {
+        video.srcObject = stream
+        video.play()
+      })
+      .catch (error => {
+        console.error(error)
+      })
+    }
   }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
